ba6222424c46f1ee04a2b244e897d9f80138567b
Your Name <joshnjoshgaming@gmail.com>
2024-03-12 12:07:50 -0400

    wrote the first commit in ~10 mins with no help, here's more idiomatic c++ using new[] and delete[] instead of malloc

--- array.hpp
+++ array.hpp
@@ -6,11 +6,11 @@
 #include <string>
 template <typename T> struct Array {
   Array() {
-    array = (T*)malloc(max * sizeof(T));
+    array = new T[max];
   }
   Array(const size_t &init_capacity) {
     max = init_capacity;
-    array = (T*)malloc(max * sizeof(T));
+    array = new T[max];
   }
   ~Array() {
     free(array);
@@ -43,25 +43,25 @@ private:
    void grow() {
     max += 100;
     std::cout << "reallocating at max size: " << max << std::endl;
-    T *array = (T*)malloc(max * sizeof(T));
-    
+    T *array = new T[max];
     for (int i = 0; i < length; ++i) {
       array[i] = this->array[i];
     }
-    free(this->array);
-    this->array = array;
+    delete[] this->array;
+    this->array = std::move(array);
   }
   void shrink() {
     max -= 100;
     std::cout << "deallocating at max size: " << max << std::endl;
-    T *array = (T*) malloc(max * sizeof(T));
+    T *array = new T[max];
     for (int i = 0; i < length; ++i) {
       array[i] = this->array[i];
     }
-    free(this->array);
-    this->array = array;
+    delete[] this->array;
+    this->array = std::move(array);
   }
-  size_t max = 10;
+  static constexpr size_t init_max = 10;
+  size_t max = init_max;
   T *array;
   size_t length = 0;
 };
