55684119fd05e4f852e73ed78278343b641a66d2
Your Name <joshnjoshgaming@gmail.com>
2024-03-12 12:31:40 -0400

    fix off by one and add some tests

--- array.hpp
+++ array.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <cstddef>
 #include <cstdlib>
-#include <iostream>
 #include <stdexcept>
 #include <string>
 template <typename T> struct Array {
@@ -13,55 +12,61 @@ template <typename T> struct Array {
     array = new T[max];
   }
   ~Array() {
-    free(array);
+    delete[] array;
   }
   
-  const int size() const { return this->length; }
-  const T get(const size_t &index) const {
-    if (index > length) {
+  const size_t Size() const { return this->length; }
+  const T Get(const size_t &index) const {
+    if (index >= length) {
       throw std::runtime_error("index out of bounds: index(" +
                                std::to_string(index) + ") length(" +
                                std::to_string(length) + ")");
     }
     return array[index];
   }
-  const void push(const T &element) {
+  const void Push(const T &element) {
     if (length >= max) {
-      grow();
+      Grow();
     }
     this->array[length++] = element;
   }
-  const T pop() {
-    auto element = this->array[length];
+  const T Pop() {
+    if (length == 0) {
+      throw std::runtime_error("Attempted to pop off an empty " + std::string("Array<") + typeid(T).name() + ">");
+    }
     length--;
+    auto element = this->array[length];
     if (length < max - 100) {
-      shrink();
+      Shrink();
     }
     return element;
   }
 private:
-   void grow() {
+   /*
+  * @breif 
+  * this function creates a new array at size new_max, moves all the elements from old_array into it, deletes the old array and
+  * returns the new updated, resized array.
+  *
+  */
+  static T *ResizeAndMove(const size_t &elementCount, const size_t &newMax, T *oldArray) {
+    T *array = new T[newMax];
+    for (int i = 0; i < elementCount; ++i) {
+      array[i] = std::move(oldArray[i]);
+    }
+    delete[] oldArray;
+    return array;
+  }
+   void Grow() {
     max += 100;
-    std::cout << "reallocating at max size: " << max << std::endl;
     T *array = new T[max];
-    for (int i = 0; i < length; ++i) {
-      array[i] = this->array[i];
-    }
-    delete[] this->array;
-    this->array = std::move(array);
+    this->array = ResizeAndMove(length, max, this->array);
   }
-  void shrink() {
+  void Shrink() {
     max -= 100;
-    std::cout << "deallocating at max size: " << max << std::endl;
-    T *array = new T[max];
-    for (int i = 0; i < length; ++i) {
-      array[i] = this->array[i];
-    }
-    delete[] this->array;
-    this->array = std::move(array);
+    this->array = ResizeAndMove(length, max, this->array);
   }
-  static constexpr size_t init_max = 10;
-  size_t max = init_max;
+  static constexpr size_t INIT_MAX = 10;
+  size_t max = INIT_MAX;
   T *array;
   size_t length = 0;
 };
--- main.cpp
+++ main.cpp
@@ -1,17 +1,49 @@
 #include "array.hpp"
+#include <cassert>
 #include <iostream>
 #include <string>
 
-int main() {
-  Array<int> ints(100);
-  ints.push(10);
-  std::cout << ints.get(0) << std::endl;
+struct Test {
+  Test(const std::string &name, const int &num) : name(name), num(num) {
+  } 
+  int num = 0;
+  std::string name;
+  void print() {
+    std::cout << "My name is : " << name << std::endl;
+  }
+};
+
+int test_struct() {
+  Array<Test*> structs(0);
   
   for (int i = 0; i < 1000; ++i) {
-    ints.push(i);
+    structs.Push(new Test("hi", i));
+  }
+  for (int i = 0; i < 1000; ++i) {
+    auto str = structs.Pop();
+    //str->print();
+  }
+  return 0;
+}
+
+int test_ints() {
+  Array<int> ints(0);
+  
+  for (int i = 0; i < 1000; ++i) {
+    ints.Push(i);
   }
   
   for(int i = 0; i < 1000; ++i) {
-    std::cout << std::to_string(ints.pop()) << std::endl;
+    ints.Pop();
   }
+  
+  return 0;
+}
+
+int main() {
+  int result = 0;
+  result += test_ints();
+  result += test_struct();
+  std::cout << "test results : " << result << std::endl;
+  std::cout << "0 means passing, otherwise the count is how many tests failed." << std::endl;
 }
